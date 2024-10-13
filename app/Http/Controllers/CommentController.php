dc36bde48bb331998b5505c131e34d02d9f870c0
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2024-10-13 16:33:08 +0300

    add validator class for store and delete comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Events\NewCommentNotificationEvent;
 use App\Helpers\ApiResponse;
+use App\Http\Requests\DeleteCommentRequest;
+use App\Http\Requests\StoreCommentRequest;
 use App\Http\Resources\CommentResource;
 use App\Http\Traits\Slugable;
 use App\Models\Comment;
@@ -16,19 +18,8 @@ use Illuminate\Support\Facades\Validator;
 class CommentController extends Controller
 {
     use Slugable;
-    public function store(Request $request)
+    public function store(StoreCommentRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'slug' => ['required', 'string', 'exists:posts,slug'],
-            'comment' => ['required', 'string'],
-        ], [], [
-            'slug' => 'Slug',
-            'comment' => 'Comment',
-        ]);
-
-        if ($validator->fails()) {
-            return ApiResponse::sendResponse(400, 'Validation Error', $validator->errors()->messages());
-        }
 
         $post = Post::where('slug', $request->slug)->first();
 
@@ -45,17 +36,8 @@ class CommentController extends Controller
     }
 
 
-    public function delete(Request $request)
+    public function delete(DeleteCommentRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'slug' => ['required', 'string', 'exists:comments,slug'],
-        ], [], [
-            'slug' => 'Slug',
-        ]);
-
-        if ($validator->fails()) {
-            return ApiResponse::sendResponse(400, 'Validation Error', $validator->errors()->messages());
-        }
 
         $comment = Comment::where('slug', $request->slug)->first();
 
---
+++ app/Http/Requests/StoreCommentRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'slug' => ['required', 'string', 'exists:posts,slug'],
+            'comment' => ['required', 'string'],
+        ];
+    }
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'slug' => 'Post Slug',
+            'comment' => 'Comment',
+        ];
+    }
+}
